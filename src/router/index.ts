f6d976c81d8d92f1fbfb7778841188bfc9f1838d
wangbing <wangbing>
2023-12-01 16:54:38 +0800

    增加elementuiplus的配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
  * @Author: wangbing wangbing
  * @Date: 2023-08-04 11:37:04
  * @LastEditors: wangbing wangbing
- * @LastEditTime: 2023-11-30 10:56:20
+ * @LastEditTime: 2023-11-30 18:10:03
  * @FilePath: \myblog\src\router\index.ts
  * @Description: 
  * 
@@ -10,7 +10,7 @@
  */
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Layout from '@/layouts/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -20,19 +20,39 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/404',
+      name: '404',
+      component: () => import('../views/404.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
+      component: Layout,
+      redirect: '/home/index',
+      children: [
+        {
+          path: '/FirstPage',
+          name: 'FirstPage',
+          component: () => import('../views/FirstPage.vue'),
+          meta: { title: '状态监控', icon: 'dashboard' }
+        }
+      ]
+    }, 
+
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // },
     {
       path: '/FirstPage',
       name: 'FirstPage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/FirstPage.vue')
     },
     {
@@ -40,11 +60,7 @@ const router = createRouter({
       name: 'ArticleDetails',
       component: () => import('../views/Blog/ArticleDetails.vue')
     },
-    {
-      path: '/404',
-      name: '404',
-      component: () => import('../views/404.vue')
-    }
+    
   ]
 })
 
