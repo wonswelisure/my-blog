4084aa99a0325a8da14892a3f35a8a19ea03a3f2
wangbing <wangbing>
2023-12-02 17:55:33 +0800

    layout的内容提交。

---
+++ src/layouts/component/Navbar.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Author: wangbing wangbing
+ * @Date: 2023-12-02 17:36:35
+ * @LastEditors: wangbing wangbing
+ * @LastEditTime: 2023-12-02 17:39:31
+ * @FilePath: \myblog\src\layouts\component\Navbar.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+-->
+<template>
+  <div class="navbar"></div>
+</template>
+  
+  <script setup lang="ts">
+// import { reactive, ref } from 'vue'
+</script>
+  
+  <style lang="scss" scoped>
+.navbar {
+  height: 59px;
+  overflow: hidden;
+  position: relative;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  width: 100%;
+  background-color: #f7f7f7;
+}
+</style>
+  
